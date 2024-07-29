e068453f59195986ed4abba0320123b0d8070fb8
Marcelo Bastos <marceloviniciusbs17@gmail.com>
2024-07-29 11:07:19 -0300

    feat: ajustes do css

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Noticia = ({ title, author, text, date }) => {
   return (
-    <div className="bg-white rounded-2xl shadow-md overflow-hidden w-full max-w-4xl m-4 transform transition-transform duration-300 hover:scale-105 p-6">
+    <div className="bg-white rounded-2xl shadow-md overflow-hidden w-full max-w-custom m-4 transform transition-transform duration-300 hover:scale-105 p-6">
       <div className="h-full flex flex-col justify-between">
         <h2 className="text-lg font-semibold text-black text-base mb-2">{title}</h2>
         <p className="text-black text-sm font-normal mb-2">{text}</p>
--- src/components/Noticias.jsx
+++ src/components/Noticias.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import Noticia from './Noticia';
 import './noticias.css';
+import NoticiasLogo from '../assets/NOTÍCIAS.png';
 
 const Noticias = () => {
   const [noticias, setNoticias] = useState([]);
@@ -26,6 +29,12 @@ const Noticias = () => {
 
   return (
     <div id='noticias' className="py-10 min-h-[105vh] flex flex-col items-center" style={{ backgroundColor: '#DFEFA6' }}>
+      <div className="w-full flex justify-between items-start mb-12">
+        <img src={NoticiasLogo} alt="Imagem" className="pl-24 pt-8" />
+        <a href="/another-page" className="link-custom mt-8 mr-24">
+          Ver todas as notícias <FontAwesomeIcon icon={faArrowRight} className="icon-custom" />
+        </a>
+      </div>
       {noticias.length > 0 ? (
         noticias.map((noticia, index) => (
           <Noticia 
