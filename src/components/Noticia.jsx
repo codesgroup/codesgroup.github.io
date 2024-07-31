ceada55152f1d775f6dd9bb61a81f5931fdff99f
Marcelo Bastos <marceloviniciusbs17@gmail.com>
2024-07-31 16:12:11 -0300

    style: card de noticia

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,4 +1,3 @@
-// Noticia.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -7,13 +6,11 @@ const Noticia = ({ id, title, author, text, date }) => {
     <Link to={`/news/${id}`} className="news-item" style={{ textDecoration: 'none', color: 'inherit' }}>
       <div className="bg-white rounded-2xl shadow-md overflow-hidden w-full max-w-custom m-4 transform transition-transform duration-300 hover:scale-105 p-6">
         <div className="h-full flex flex-col justify-between">
-          <h2 className="text-lg font-semibold text-black text-base mb-2">{title}</h2>
-          <div className="flex justify-between items-center">
-           
+          <div className="flex justify-between items-center mb-2">
+            <h2 className="text-lg font-semibold text-black text-base">{title}</h2>
             <p className="text-gray-500 text-xs">{date}</p>
           </div>
           <p className="noticia-text text-black text-sm font-normal mb-2">{text}</p>
-         
         </div>
       </div>
     </Link>
